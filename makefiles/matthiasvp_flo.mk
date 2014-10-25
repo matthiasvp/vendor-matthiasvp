39e372f09bb940f48c675ee0e6692196b69aa75a
matthiasvp <matthiasvp@gmail.com>
2014-10-25 19:12:11 +0200

    Add makefile and bootanimation for flo

---
+++ makefiles/matthiasvp_flo.mk
@@ -0,0 +1,9 @@
+#Inherit configuration from AOSP
+$(call inherit-product, device/asus/flo/aosp_flo.mk)
+
+#Set device specific configuration
+PRODUCT_NAME := matthiasvp_flo
+PRODUCT_MODEL:= Nexus 7
+
+#Add bootanimation to build
+PRODUCT_COPY_FILES += vendor/matthiasvp/bootanimation/flo/bootanimation.zip:system/media/bootanimation.zip
